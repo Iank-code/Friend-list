ad792104e7c93b4c379ab596f26aa9c9aaaee38c
ianK-code <kian99564@gmail.com>
2023-06-02 21:06:09 +0300

    Added friends scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_150153) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_180019) do
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "homes", force: :cascade do |t|
     t.string "index"
     t.datetime "created_at", null: false
